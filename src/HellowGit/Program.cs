774aeec25a41de51c77df6afa41caf7cccfbdb93
Tinashe Nyamuranga <tinanyamuranga@gmail.com>
2024-05-10 18:06:23 +0200

    fgtgdj

--- src/HellowGit/Program.cs
+++ src/HellowGit/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World from tinashe!");
+Console.WriteLine("Hello, World from nashe!");
 
 Console.ReadLine();

181f09fdc5e53410b465a318e7d1982f9230647c
Moses Wankis <mwankis@gmail.com>
2024-06-26 20:20:49 +0200

    added files

---
+++ src/Hap.Workflows/GetOptionSetValueFromLabel.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+
+namespace Hap.Workflows
+{
+    public class GetOptionSetValueFromLabel : CodeActivity
+    {
+
+
+        [RequiredArgument]
+        [Input("Text")]
+        public InArgument<string> Text { get; set; }
+
+        [RequiredArgument]
+        [Input("EntityName")]
+        public InArgument<string> EntityName { get; set; }
+
+        [RequiredArgument]
+        [Input("FieldName")]
+        public InArgument<string> FieldName { get; set; }
+
+        [Output("OptionSetValue")]
+        public OutArgument<int> OptionSetValue { get; set; }
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            var context = executionContext.GetExtension<IWorkflowContext>();
+            var serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+            var organizationService = serviceFactory.CreateOrganizationService(null);
+
+            var text = Text.Get(executionContext);
+            var entityName = EntityName.Get(executionContext);
+            var fieldName = FieldName.Get(executionContext);
+            var optionSetValue = GetOptionSetValue(organizationService, entityName, fieldName, text);
+
+            executionContext.SetValue(OptionSetValue, optionSetValue);
+        }
+
+        public int GetOptionSetValue(IOrganizationService service, string entityName, string fieldName, string text)
+        {
+            try
+            {
+                var retrieveAttributeRequest = new RetrieveAttributeRequest
+                {
+                    EntityLogicalName = entityName,
+                    LogicalName = fieldName,
+                    RetrieveAsIfPublished = true
+                };
+
+                var retrieveAttributeResponse = (RetrieveAttributeResponse)service.Execute(retrieveAttributeRequest);
+                var retrievedPicklistAttributeMetadata = (PicklistAttributeMetadata)retrieveAttributeResponse.AttributeMetadata;
+                OptionMetadata[] optionList = retrievedPicklistAttributeMetadata.OptionSet.Options.ToArray();
+                foreach (OptionMetadata oMD in optionList)
+                {
+                    if (oMD.Label.LocalizedLabels[0].Label.ToString().ToLower() == text.ToLower())
+                    {
+                        var selectedOptionValue = oMD.Value.Value;
+                        return selectedOptionValue;
+
+                    }
+                }
+                return -1;
+            }
+            catch (System.Exception)
+            {
+                return -1;
+            }
+
+        }
+    }
+}
--- src/HellowGit/Program.cs
+++ src/HellowGit/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World from moses");
 
+// object datetime string obj
+
+var stringbhd = "05/02/2005";
+ var yrbhd = DateTime.Parse(stringbhd);
+//
 Console.ReadLine();
